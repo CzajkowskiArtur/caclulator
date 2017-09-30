6af7549a20c095d2d17bce2e529e4cb38164dac7
CzajkowskiArtur <artur.piotr.czajkowski@gmail.com>
2017-09-30 20:47:38 +0200

    *

---
+++ Actions.java
@@ -0,0 +1,21 @@
+package pl.sdacademy.Math;
+
+/**
+ * Created by Artur on 27.08.2017.
+ */
+public interface Actions {
+
+    void add(double x);
+
+    void subtract(double x);
+
+    void multiplication(double x);
+
+    void division(double x);
+
+    void sqrt (double x);
+
+    void pot (double x);
+
+}
+
